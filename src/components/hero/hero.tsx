119cc6f8f36867335cd9bdd473403977dd793b4d
Amit Pal <beamitpal@gmail.com>
2024-02-18 19:18:14 +0530

    some more
    some more

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -14,7 +14,13 @@ function Hero() {
     <>
       <section className="text-gray-600 bg-yellow-500 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-         <Image src="/hero/hero.webp" className="rounded" alt="hero" width={500} height={500} />
+          <Image
+            src="/hero/hero.webp"
+            className="rounded"
+            alt="hero"
+            width={500}
+            height={500}
+          />
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Welcome To Sankat Mochan Group Of Institutions Best School in
@@ -25,10 +31,12 @@ function Hero() {
               Welcome to Sankat Mochan Group of Institutions! Embrace excellence
               in education with us. Explore a holistic learning environment that
               nurtures minds and shapes futures. For inquiries, admissions, or
-              assistance, contact us at <strong className="text-gray-900">contact@smgoi.com</strong>  or call <strong className="text-gray-900">+91
-              8960788977</strong>. Step into our campus at Kariya Bajhna,Sultanpur,Uttar
-              Pradesh and embark on a journey of knowledge and growth. Your
-              education, your future &hyphen; let's create it together!
+              assistance, contact us at
+              <strong className="text-gray-900">contact@smgoi.com</strong> or
+              call <strong className="text-gray-900">+91 8960788977</strong>.
+              Step into our campus at Kariya Bajhna,Sultanpur,Uttar Pradesh and
+              embark on a journey of knowledge and growth. Your education, your
+              future &hyphen; let&lsquo;s create it together!
             </p>
             <div className="flex justify-center">
               <button
