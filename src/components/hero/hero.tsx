524ce709e9be838c5e42816dea422e438ea728d5
Amit Pal <beamitpal@gmail.com>
2024-02-18 21:52:24 +0530

    some
    some more

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -20,6 +20,7 @@ function Hero() {
             alt="hero"
             width={500}
             height={500}
+            loading="eager"
           />
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
