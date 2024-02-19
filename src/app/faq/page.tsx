2cddeb048a29157438c0c3a706b0e1dafc88e1bb
Amit Pal <beamitpal@gmail.com>
2024-02-19 09:37:29 +0530

    some seo changes
    some seo changes

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { Metadata } from "next";
-import Head from "next/head";
 export const metadata: Metadata = {
   title:{
     absolute:"FAQs | Sankat Mochan Group Of Institutions | Sultanpur | Uttar Pradesh (U.P.)",
@@ -67,31 +66,6 @@ export default function faq() {
 
   return (
     <>
-     <Head>
-        <meta property="og:url" content="https://smgoi.com" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="FAQs | Sankat Mochan Group Of Institutions | Sultanpur | Uttar Pradesh (U.P.)"
-        />
-        <meta
-          property="og:description"
-          content="Welcome to Sankat Mochan Group of Institutions, an academic haven situated in the tranquil town of Kariya Bajhna, Sultnapur, Uttar Pradesh, with the zip code 228142. Affiliated with the esteemed Central Board of Secondary Education (CBSE), our institution stands as a symbol of educational excellence, shaping the destinies of young minds.  Immerse yourself in a holistic educational experience against the scenic backdrop of Uttar Pradesh. Sankat Mochan Group of Institutions blends academic prowess, artistic expression, and athletic achievement, crafting a comprehensive learning environment that extends beyond conventional classrooms.  Discover a passion for learning with our dedicated faculty, committed to igniting curiosity, fostering creativity, and encouraging critical thinking. Our campus boasts modern facilities, including well-appointed classrooms, advanced laboratories, and expansive sports fields, ensuring a conducive setting for academic and personal development.  Beyond academic rigor, we emphasize character development, instilling values of compassion, responsibility, and global awareness. As proud affiliates of the CBSE board, our curriculum aligns with national standards, preparing students for a future of academic success and competitive excellence.  Sankat Mochan Group of Institutions is more than just a school—it's a community dedicated to unlocking the potential within each student. Join us on this educational journey, where every day presents an opportunity to learn, grow, and thrive. Together, let's pave the way for a brighter future, nurturing the leaders and innovators of tomorrow."
-        />
-        <meta property="og:image" content="https://smgoi.com/backseo.png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="smgoi.com" />
-        <meta property="twitter:url" content="https://smgoi.com" />
-        <meta
-          name="twitter:title"
-          content="FAQs | Sankat Mochan Group Of Institutions | Sultanpur | Uttar Pradesh (U.P.)"
-        />
-        <meta
-          name="twitter:description"
-          content="Welcome to Sankat Mochan Group of Institutions, an academic haven situated in the tranquil town of Kariya Bajhna, Sultnapur, Uttar Pradesh, with the zip code 228142. Affiliated with the esteemed Central Board of Secondary Education (CBSE), our institution stands as a symbol of educational excellence, shaping the destinies of young minds.  Immerse yourself in a holistic educational experience against the scenic backdrop of Uttar Pradesh. Sankat Mochan Group of Institutions blends academic prowess, artistic expression, and athletic achievement, crafting a comprehensive learning environment that extends beyond conventional classrooms.  Discover a passion for learning with our dedicated faculty, committed to igniting curiosity, fostering creativity, and encouraging critical thinking. Our campus boasts modern facilities, including well-appointed classrooms, advanced laboratories, and expansive sports fields, ensuring a conducive setting for academic and personal development.  Beyond academic rigor, we emphasize character development, instilling values of compassion, responsibility, and global awareness. As proud affiliates of the CBSE board, our curriculum aligns with national standards, preparing students for a future of academic success and competitive excellence.  Sankat Mochan Group of Institutions is more than just a school—it's a community dedicated to unlocking the potential within each student. Join us on this educational journey, where every day presents an opportunity to learn, grow, and thrive. Together, let's pave the way for a brighter future, nurturing the leaders and innovators of tomorrow."
-        />
-        <meta name="twitter:image" content="https://smgoi.com/backseo.png" />
-      </Head>
       <section className="py-14">
         <div className="max-w-screen-xl mx-auto px-4 md:px-8">
           <div className="max-w-lg">
